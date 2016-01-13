0bbbccf4befe50334f6ad2ae8b24b06a4b4f094f
Italo Cunha <cunha@dcc.ufmg.br>
2016-01-13 20:18:29 -0200

    update openvpn scripts

--- openvpn/scripts/down.sh
+++ openvpn/scripts/down.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 set -eu
 
-TUN_DEV=$1
-TUN_MTU=$2
-LINK_MTU=$3
-IFCONFIG_LOCAL_IP=$4
-IFCONFIG_REMOTE_IP=$5
-INIT_RESTART=$6
+dev=$1
+dev_mtu=$2
+link_mtu=$3
+local_ip=$4
+remote_ip=$5
+init_restart=$6
 
 echo "down $0 $1 $2 $3 $4 $5 $6" >> logs/up-down.log
+echo "down" > logs/$dev.status
--- openvpn/scripts/up.sh
+++ openvpn/scripts/up.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 set -eu
 
-TUN_DEV=$1
-TUN_MTU=$2
-LINK_MTU=$3
-IFCONFIG_LOCAL_IP=$4
-IFCONFIG_REMOTE_IP=$5
-INIT_RESTART=$6
+dev=$1
+dev_mtu=$2
+link_mtu=$3
+local_ip=$4
+remote_ip=$5
+init_restart=$6
 
 echo "up $0 $1 $2 $3 $4 $5 $6" >> logs/up-down.log
+echo "up $dev $local_ip" > logs/$dev.status
 
-echo 0 > /proc/sys/net/ipv4/conf/$TUN_DEV/rp_filter
+echo 0 > /proc/sys/net/ipv4/conf/$dev/rp_filter
