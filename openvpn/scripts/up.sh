e7be6bfaacf9fadbb64217bd1c5f5c77fc584a47
Italo Cunha <cunha@dcc.ufmg.br>
2016-04-23 21:21:00 -0300

    disabling rp-filter as we lack data plane

--- openvpn/scripts/up.sh
+++ openvpn/scripts/up.sh
@@ -11,4 +11,4 @@ init_restart=$6
 echo "up $1 $2 $3 $4 $5 $6" >> logs/up-down.log
 echo "up $local_ip" > logs/$dev.status
 
-echo 0 > /proc/sys/net/ipv4/conf/$dev/rp_filter
+# echo 0 > /proc/sys/net/ipv4/conf/$dev/rp_filter
