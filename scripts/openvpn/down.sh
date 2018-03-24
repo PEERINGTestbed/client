7c61b7d2953febf80b58ab13c0e2cf16d28db2a1
Italo Cunha <cunha@dcc.ufmg.br>
2018-03-24 01:27:00 -0300

    shellcheck improvements + openvpn sock

--- scripts/openvpn/down.sh
+++ scripts/openvpn/down.sh
@@ -9,4 +9,4 @@ remote_ip=$5
 init_restart=$6
 
 echo "down $1 $2 $3 $4 $5 $6" >> var/up-down.log
-echo "down" > var/$dev.status
+echo "down" > "var/$daemon_name.updown"
--- scripts/openvpn/up.sh
+++ scripts/openvpn/up.sh
@@ -8,8 +8,8 @@ local_ip=$4
 remote_ip=$5
 init_restart=$6
 
-echo "cmdline $@" >> var/up-down.log
-echo "$(set)" >> var/up-down.log
-echo "up $local_ip $ifconfig_ipv6_local" > var/$dev.status
+echo "cmdline $*" >> var/up-down.log
+set >> var/up-down.log
+echo "up $local_ip $ifconfig_ipv6_local" > "var/$daemon_name.updown"
 
 # echo 0 > /proc/sys/net/ipv4/conf/$dev/rp_filter
