96a7b4f5f0e18066dad2000d3c3078d33a56628f
Italo Cunha <cunha@dcc.ufmg.br>
2019-11-25 17:54:04 -0500

    update bird-route-parser to python3

--- utils/bird-route-parser/parse.py
+++ utils/bird-route-parser/parse.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import argparse
 import gzip
@@ -11,10 +11,27 @@ import resource
 import parsers.util as util
 import parsers.protocols as proto
 import parsers.route as route
-from cblr import CachingBufferedLineReader
 
 
-def show_protocols(reader, outfd):  # {{{
+class CachingBufferedLineReader:
+    def __init__(self, fd):
+        self.fd = fd
+        self.line = None
+        self.read = True
+
+    def readline(self):
+        if self.read:
+            self.line = self.fd.readline()
+        else:
+            self.read = True
+            assert self.line is not None
+        return self.line
+
+    def rewind_line(self):
+        self.read = False
+
+
+def show_protocols(reader, outfd):
     header = reader.readline()
     assert header.split() == proto.HEADER_LINE_FIELDS
     results = list()
@@ -22,109 +39,107 @@ def show_protocols(reader, outfd):  # {{{
     while line:
         m = re.match(proto.SUMMARY_RE, line)
         if not m:
-            logging.debug('parse_proto: no match [%s]', line)
+            logging.debug("parse_proto: no match [%s]", line)
             line = reader.readline()
             continue
         line = reader.readline()
-        if m.group('proto') not in proto.SUPPORTED:
-            logging.debug('parse_proto: %s not supported', m.group('proto'))
+        if m.group("proto") not in proto.SUPPORTED:
+            logging.debug("parse_proto: %s not supported", m.group("proto"))
             continue
         result = m.groupdict()
-        v = util.parse_desc_lines(reader, proto.DETAILS_RE,
-                                  proto.DETAILS_PARSERS)
-        result['details'] = v
+        v = util.parse_desc_lines(reader, proto.DETAILS_RE, proto.DETAILS_PARSERS)
+        result["details"] = v
         results.append(result)
     json.dump(results, outfd, indent=2)
     return results
-# }}}
 
 
-def show_route(reader, outfd, return_json=False):  # {{{
+def show_route(reader, outfd, return_json=False):
     routes = list()
     network = None
     line = reader.readline()
     while line:
         m = re.match(route.SUMMARY_RE, line)
-        assert m, '%s' % line
+        assert m, "%s" % line
         if m.group(route.SUMMARY_NETWORK_KEY) is not None:
             network = m.group(route.SUMMARY_NETWORK_KEY)
         rt = m.groupdict()
         rt[route.SUMMARY_NETWORK_KEY] = network
         rt = dict((k, v.strip()) for k, v in rt.items() if v is not None)
-        v = util.parse_desc_lines(reader, route.DETAILS_RE,
-                                  route.DETAILS_PARSERS, route.SUMMARY_RE)
-        rt['attributes'] = v
+        v = util.parse_desc_lines(
+            reader, route.DETAILS_RE, route.DETAILS_PARSERS, route.SUMMARY_RE
+        )
+        rt["attributes"] = v
         line = reader.readline()
         json.dump(rt, outfd)
         if line:
-            outfd.write('\n')
+            outfd.write("\n")
         if return_json:
             routes.append(rt)
     return routes
-# }}}
 
 
-def create_parser():  # {{{
-    desc = '''Parse BIRD output'''
-
+def create_parser():
+    desc = """Parse BIRD output into JSON data"""
     cmdparser = argparse.ArgumentParser(description=desc)
-
-    cmdparser.add_argument('--in',
-                           dest='infn',
-                           metavar='FILE',
-                           required=True,
-                           type=str,
-                           help='Input file (with BIRD output), - for STDIN')
-
-    cmdparser.add_argument('--out',
-                           dest='outfn',
-                           metavar='FILE',
-                           required=True,
-                           type=str,
-                           help='Output file (JSON)')
-
+    cmdparser.add_argument(
+        "--in",
+        dest="infn",
+        metavar="FILE",
+        required=True,
+        type=str,
+        help="Input file (with BIRD output), - for STDIN",
+    )
+    cmdparser.add_argument(
+        "--out",
+        dest="outfn",
+        metavar="FILE",
+        required=True,
+        type=str,
+        help="Output file (JSON)",
+    )
     cmd = cmdparser.add_mutually_exclusive_group(required=True)
-    cmd.add_argument('--protocols',
-                     dest='parser',
-                     action='store_const',
-                     const=show_protocols,
-                     help='Parse the output of `show protocols`')
-    cmd.add_argument('--route',
-                     dest='parser',
-                     action='store_const',
-                     const=show_route,
-                     help='Parse the output of `show route`')
-
+    cmd.add_argument(
+        "--protocols",
+        dest="parser",
+        action="store_const",
+        const=show_protocols,
+        help="Parse the output of `show protocols`",
+    )
+    cmd.add_argument(
+        "--route",
+        dest="parser",
+        action="store_const",
+        const=show_route,
+        help="Parse the output of `show route`",
+    )
     return cmdparser
-# }}}
 
 
-def main():  # {{{
+def main():
     resource.setrlimit(resource.RLIMIT_AS, (1 << 33, 1 << 33))
     resource.setrlimit(resource.RLIMIT_FSIZE, (1 << 35, 1 << 35))
-    logging.basicConfig(filename='parse.log', format='%(message)s',
-                        level=logging.NOTSET)
+    logging.basicConfig(format="%(message)s", level=logging.NOTSET)
 
     cmdparser = create_parser()
     opts = cmdparser.parse_args()
 
-    if opts.infn == '-':
+    if opts.infn == "-":
         opts.fd = sys.stdin
     else:
-        opts.fd = open(opts.infn, 'r')
+        opts.fd = open(opts.infn, "r")
     reader = CachingBufferedLineReader(opts.fd)
 
-    if opts.outfn.endswith('.gz'):
-        outfd = gzip.open(opts.outfn, 'w+')
+    if opts.outfn.endswith(".gz"):
+        outfd = gzip.open(opts.outfn, "w+")
     else:
-        outfd = open(opts.outfn, 'w+')
+        outfd = open(opts.outfn, "w+")
 
     opts.parser(reader, outfd)
 
     opts.fd.close()
     outfd.close()
-# }}}
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     sys.exit(main())
--- utils/bird-route-parser/parsers/util.py
+++ utils/bird-route-parser/parsers/util.py
@@ -6,38 +6,35 @@ def normalize_desc(string):
     return string.strip()  # .lower().replace(' ', '_')
 
 
-def set_desc_values(result, match):  # {{{
+def set_desc_values(result, match):
     v = match.groupdict()
-    result[normalize_desc(v['desc'])] = v
-    del v['desc']
-# }}}
+    result[normalize_desc(v["desc"])] = v
+    del v["desc"]
 
 
-def parse_desc_colon_int(reader):  # {{{
-    REGEX = r'^\s+(?P<desc>[^:]+):\s+(?P<data>\d+)$'
+def parse_desc_colon_int(reader):
+    REGEX = r"^\s+(?P<desc>[^:]+):\s+(?P<data>\d+)$"
     line = reader.readline()
     m = re.match(REGEX, line)
-    return normalize_desc(m.group('desc')), int(m.group('data'))
-# }}}
+    return normalize_desc(m.group("desc")), int(m.group("data"))
 
 
-def parse_desc_colon_str(reader):  # {{{
-    REGEX = r'^\s+(?P<desc>[^:]+):(?P<data>.*)$'
+def parse_desc_colon_str(reader):
+    REGEX = r"^\s+(?P<desc>[^:]+):(?P<data>.*)$"
     line = reader.readline().rstrip()
     lines = [line]
     line = reader.readline()
-    while line and line.startswith('\t\t'):
+    while line and line.startswith("\t\t"):
         lines.append(line.strip())
         line = reader.readline()
     reader.rewind_line()
-    buf = ''.join(l for l in lines)
+    buf = "".join(l for l in lines)
     m = re.match(REGEX, buf)
-    data = str(m.group('data')).strip()
-    return normalize_desc(m.group('desc')), data
-# }}}
+    data = str(m.group("data")).strip()
+    return normalize_desc(m.group("desc")), data
 
 
-def parse_desc_lines(reader, regex, parsers, ignore_regex=None):  # {{{
+def parse_desc_lines(reader, regex, parsers, ignore_regex=None):
     result = dict()
     line = reader.readline()
     while line:
@@ -46,17 +43,15 @@ def parse_desc_lines(reader, regex, parsers, ignore_regex=None):  # {{{
         m = re.match(regex, line)
         if not m:
             break
-        desc = m.group('desc')
+        desc = m.group("desc")
         # allow parsers to be an instance of defaultdict:
         reader.rewind_line()
         try:
             k, v = parsers[desc](reader)
             result[k] = v
         except KeyError:
-            logging.exception('parse_details: desc [%s] has no parser [%s]',
-                              desc, line)
+            logging.exception("parse_details: desc [%s] has no parser [%s]", desc, line)
             raise
         line = reader.readline()
     reader.rewind_line()
     return result
-# }}}
