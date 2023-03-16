c3576d76b3fb94fdcb9de862162a4a07cb4cb891
Italo Cunha <cunha@dcc.ufmg.br>
2023-03-16 00:30:20 -0300

    extend dump-peer-routes to v6

--- utils/dump-peer-routes.sh
+++ utils/dump-peer-routes.sh
@@ -1,21 +1,44 @@
-#!/bin/sh
+#!/bin/bash
+set -eu
 
 progdir=$(cd "$(dirname "$0")" && pwd -P)
 
-BIRD_SOCKET=$(readlink -e "$progdir/../var/bird.ctl")
-OUTPUT_FILE=$(pwd)/dump.json.gz
+BIRD4_SOCKET=$(readlink -e "$progdir/../var/bird.ctl")
+BIRD6_SOCKET=$(readlink -e "$progdir/../var/bird6.ctl")
 
-if [ ! -e "$BIRD_SOCKET" ] ; then
-    echo "BIRD socket not found at $BIRD_SOCKET"
-fi
+OUTPUT4_PREFIX=$(pwd)/dump4
+OUTPUT6_PREFIX=$(pwd)/dump6
 
-echo "Dumping peer routes from BIRD instance on $BIRD_SOCKET"
-echo "into $OUTPUT_FILE"
+function get_table {
+    local socket=$1
+    local outfn=$2
+    local birdc=$3
 
-# Note: birdc prints some trash on the beginning of its "bird>" prompt.
-# We use sed to get rid of it.
-echo "show route table rtup all" | sudo birdc -r -s "$BIRD_SOCKET" \
-        | sed 's/^bird> \r\x1b\[K//' \
-        | tail -n +4 \
-        | "$progdir/bird-route-parser/parse.py" --in - --out "$OUTPUT_FILE" \
-                --route
+    if [[ ! -e "$socket" ]] ; then
+        echo "BIRD socket not found at $socket"
+        return
+    fi
+
+    echo "Dumping routes from BIRD instance on $socket into $outfn"
+
+    # Note: birdc prints some trash on the beginning of its "bird>" prompt.
+    # We use sed to get rid of it.
+    echo "show route table rtup all" | sudo "$birdc" -r -s "$socket" \
+            | sed 's/^bird> \r\x1b\[K//' \
+            | tail -n +4 \
+            | gzip \
+            > "$outfn"
+}
+
+function parse_table {
+    local infn=$1
+    local outfn=$2
+    echo "Parsing $infn -> $outfn"
+    "$progdir/bird-route-parser/parse.py" --in "$infn" --out "$outfn" \
+            --bird1 --route
+}
+
+get_table "$BIRD4_SOCKET" "$OUTPUT4_PREFIX.table.gz" birdc
+get_table "$BIRD6_SOCKET" "$OUTPUT6_PREFIX.table.gz" birdc6
+parse_table "$OUTPUT4_PREFIX.table.gz" "$OUTPUT4_PREFIX.json.gz"
+parse_table "$OUTPUT6_PREFIX.table.gz" "$OUTPUT6_PREFIX.json.gz"
