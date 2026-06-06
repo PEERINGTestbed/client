f3f557a05a1d1a86bb50043f28b937ce0b8d78bd
Italo Cunha <cunha@dcc.ufmg.br>
2026-06-06 18:40:53 +0000

    maint: simplify defs.py

--- utils/experiments/estimate-unicast-latency/defs.py
+++ utils/experiments/estimate-unicast-latency/defs.py
@@ -1,13 +1,8 @@
 from ipaddress import IPv4Network, IPv6Network
 import pathlib
 
-ANNOUNCEMENT_DURATION = 5400
-PROPAGATION_TIME = 600
-PER_PFX_PPS = 600
-
 BASEDIR = pathlib.Path(__file__).resolve().parents[3]
 TARGETS_FILE = pathlib.Path("data/targets.txt")
-
 PREFIXES: list[IPv4Network | IPv6Network] = [
     IPv4Network("184.164.226.0/24"),
     IPv4Network("184.164.227.0/24"),
@@ -15,15 +10,7 @@ PREFIXES: list[IPv4Network | IPv6Network] = [
     IPv4Network("184.164.254.0/24"),
 ]
 
-VULTR_PROVIDERS_20251103 = [
-    174,
-    1299,
-    2914,
-    3257,
-    3356,
-    3491,
-    4755,
-    7922,
-    9498,
-]
-VULTR_PROVIDERS = VULTR_PROVIDERS_20251103
+ANNOUNCEMENT_DURATION = 5400
+PROPAGATION_TIME = 600
+PER_PFX_PPS = 600
+
